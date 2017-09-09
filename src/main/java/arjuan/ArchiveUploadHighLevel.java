b7f8ec22238e13d9a2d52984add142970951605d
arjuan <arjuan@gmail.com>
2017-09-09 14:05:45 +0300

    changed system out into log calls

--- src/main/java/arjuan/ArchiveUploadHighLevel.java
+++ src/main/java/arjuan/ArchiveUploadHighLevel.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.amazonaws.event.ProgressListener;
 import com.amazonaws.event.ProgressEvent;
 import com.amazonaws.event.ProgressEventType;
@@ -17,15 +20,17 @@ public class ArchiveUploadHighLevel {
     
 	public static String region     	= null;
 	public static String account		= null;
-	public static String vault		= null;
-    	public static String archive		= null;
-    	public static String dir		= null;
-    	public static String description	= null;
-	    
-        public static void main(String[] args) throws IOException {
-		
-                // Initialize all needed values per the system properties
-		init();
+	public static String vault			= null;
+	public static String archive		= null;
+	public static String dir			= null;
+	public static String description	= null;
+	
+	static final Log log = LogFactory.getLog(ArchiveTransferManager.class);
+
+	public static void main(String[] args) throws IOException {
+	
+			// Initialize all needed values per the system properties
+	init();
 		
 		// load the credentials from the .aws profile
     	        ProfileCredentialsProvider credentials = new ProfileCredentialsProvider();
@@ -38,18 +43,18 @@ public class ArchiveUploadHighLevel {
                 // if given, concatenate dir to archive name
                 String fileName = (dir == null) ? archive : dir + File.separatorChar + archive;
 
-                System.out.println("File (archive)      : " + fileName);
-                System.out.println("Description         : " + description);
-                System.out.println("Region              : " + region);
-                System.out.println("Account             : " + account);
-                System.out.println("Vault               : " + vault);
+				log.info("File (archive): " + fileName);
+                log.info("Description: " + description);
+                log.info("Region: " + region);
+                log.info("Account: " + account);
+                log.info("Vault: " + vault);
 	
                 try {
                         File file = new File(fileName);
                         ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
                         UploadResult result = atm.upload(account, vault, description, file , new UploadProgressListener(file.length()));
                         
-                        System.out.println(new Date() + "\t" + "Done! Archive ID: " + result.getArchiveId());
+                        log.info(new Date() + "\t" + "Done! Archive ID: " + result.getArchiveId());
                     
                 } catch (Exception e) {
                     System.err.println(e);
@@ -92,27 +97,27 @@ public class ArchiveUploadHighLevel {
 
 	private static class UploadProgressListener implements ProgressListener {
 
-                private long total      = 0L;
-                private long counter    = 0L;
-                private long partSize   = 0L;
+		private long total      = 0L;
+		private long counter    = 0L;
+		private long partSize   = 0L;
 
-                UploadProgressListener(long total) {
-                        this.total = total;
-                }
+		UploadProgressListener(long total) {
+			this.total = total;
+		}
 
 		@Override
 		public void progressChanged(ProgressEvent progressEvent) {
 
 			if (progressEvent.getEventType() == ProgressEventType.REQUEST_CONTENT_LENGTH_EVENT) {
-                                partSize = progressEvent.getBytes();
-                                System.out.println(new Date() + "\t" + "Part size: " + partSize);
-                        }
+				partSize = progressEvent.getBytes();
+				ArchiveUploadHighLevel.log.info(new Date() + "\t" + "Part size: " + partSize);
+            }
 
 			if (progressEvent.getEventType() == ProgressEventType.CLIENT_REQUEST_SUCCESS_EVENT) {
-                                counter += partSize;
-                                int percentage = (int)(counter * 100.0 / total);
-			        System.out.println(new Date() + "\t" + "Successfully transferred: " + counter + " / " + total + " (" + percentage + "%)");
-                        }
+				counter += partSize;
+				int percentage = (int)(counter * 100.0 / total);
+				ArchiveUploadHighLevel.log.info(new Date() + "\t" + "Successfully transferred: " + counter + " / " + total + " (" + percentage + "%)");
+			}
 		}
 	}
 }
